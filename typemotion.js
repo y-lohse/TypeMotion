85780eb1f2cc9cdea2efe832ac20ad2e9875abf3
Yannick Lohse <glorf.travian@gmail.com>
2013-03-17 00:06:13 +0100

    Computing the line break positions

--- typemotion.js
+++ typemotion.js
@@ -4,4 +4,23 @@ $(function(){
 	$('p').each(function(){
 		$(this).html('<span>'+$(this).text().split(' ').join('</span> <span>')+'</span>');
 	});
+	
+	//calculating teh measure
+	$('p').click(function(){
+		//finding out where the line break
+		var text = $(this).text(),
+			$spans = $(this).find('span'),
+			prevOffset = $spans.first().offset().top,
+			lineBreaks = [];
+		
+		$spans.each(function(index){
+			var currentOffset = $(this).offset().top;
+			if (currentOffset != prevOffset){
+				prevOffset = currentOffset;
+				lineBreaks.push(index);
+			}
+		});
+		
+		console.log(lineBreaks);
+	});
 });
