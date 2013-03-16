93ca184eb209ece3fc511bde8133821afd92d097
Yannick Lohse <glorf.travian@gmail.com>
2013-03-17 00:01:45 +0100

    Wrapping every word inside p tags in spans

--- typemotion.js
+++ typemotion.js
@@ -1,2 +1,7 @@
 $(function(){
+	//setup
+	//wrapping every word inside paragraphs inside spans
+	$('p').each(function(){
+		$(this).html('<span>'+$(this).text().split(' ').join('</span> <span>')+'</span>');
+	});
 });
